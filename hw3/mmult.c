44f5c6c4eafe6d050bfa1384262a126695a35c31
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-08 22:20:26 -0800

    initializing matrices to their indices, printing working correctly

--- hw3/mmult.c
+++ hw3/mmult.c
@@ -9,6 +9,7 @@
 
 int main()
 {
+
   int n, i, j, k;
   double sum = 0;
 
@@ -25,35 +26,58 @@ int main()
     return(-1);
   }
 
-  // get A and B from user
-  printf("Enter the elements of first matrix\n");
-  for (j = 0; j < n; j++)
-    for (i = 0; i < n; i++)
-      scanf("%lf", &A[i + n*j]); // using column-first indexing
+  /* // get A and B from user */
+  /* printf("Enter the elements of first matrix\n"); */
+  /* for (j = 0; j < n; j++) */
+  /*   for (i = 0; i < n; i++) */
+  /*     scanf("%lf", &A[i + n*j]); // using column-first indexing */
+  /* printf("Enter the elements of second matrix\n"); */
+  /* for (j = 0; j < n; j++) */
+  /*   for (i = 0; i < n; i++) // TODO: optimize allocation to loop nesting */
+  /*     scanf("%lf", &B[i + n*j]); // using column-first indexing */
 
-  printf("Enter the elements of second matrix\n");
-  for (j = 0; j < n; j++)
-    for (i = 0; i < n; i++)
-      scanf("%lf", &B[i + n*j]); // using column-first indexing
+  // make random matrices A and B
+  for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) { // TODO: optimize allocation to loop nesting
+      /* A[i + n*j] = random(); */
+      /* B[i + n*j] = random(); */
+      A[i + n*j] = i + n*j;
+      B[i + n*j] = i + n*j;
+    }
+  }
 
+  // perform multiplication C = AB
   for (j = 0; j < n; j++) {
     for (i = 0; i < n; i++) {
       for (k = 0; k < n; k++) {
           sum = sum + A[i + n*k]*B[k + n*j];
         }
 
-        C[j + n*i] = sum;
+        C[i + n*j] = sum;
         sum = 0;
       }
   }
 
-  printf("Product C of AB:\n");
-
-  for (j = 0; j < n; j++) {
-      for (i = 0; i < n; i++)
-        printf("%lf\t", C[i + n*j]);
+  // display A and B
+  printf("Matrix A:\n");
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++)
+        printf("%lf\t", A[i + n*j]);
       printf("\n");
-    }
+  }
+  printf("Matrix B:\n");
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++)
+        printf("%lf\t", B[i + n*j]);
+      printf("\n");
+  }
+
+  printf("Product C of AB:\n");
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++)
+      printf("%lf\t", C[i + n*j]);
+    printf("\n");
+  }
 
   // ## CLEAN UP
   free(A); // clean up unused arrays that were dynamically allocated
