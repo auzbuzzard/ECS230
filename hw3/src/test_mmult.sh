f0ab8f39eba9a98eb534730c6cb36758d6642f85
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-12 15:05:36 -0800

    wrote full mx size timing bash script

--- hw3/src/test_mmult.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../bin/mmult 100 100 > ../data/n100_r100_ord1.csv
---
+++ hw3/src/test_mmult_diff_mx_size.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+../bin/mmult 100 100 > ../data/n100_r100_ord1.csv
+../bin/mmult 200 100 > ../data/n200_r100_ord1.csv
+../bin/mmult 500 100 > ../data/n500_r100_ord1.csv
+../bin/mmult 1000 100 > ../data/n1000_r100_ord1.csv
+../bin/mmult 2000 100 > ../data/n2000_r100_ord1.csv
