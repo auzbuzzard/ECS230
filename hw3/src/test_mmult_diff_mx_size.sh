4775a55233392c1ed47726b8ec188b3375975e01
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-12 15:17:05 -0800

    limited matrix size test cases to reasonable repetitions for time constraints

--- hw3/src/test_mmult_diff_mx_size.sh
+++ hw3/src/test_mmult_diff_mx_size.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-../bin/mmult 100 100 > ../data/n100_r100_ord1.csv
+../bin/mmult 100 1000 > ../data/n100_r1000_ord1.csv
+echo "100 done"
 ../bin/mmult 200 100 > ../data/n200_r100_ord1.csv
-../bin/mmult 500 100 > ../data/n500_r100_ord1.csv
-../bin/mmult 1000 100 > ../data/n1000_r100_ord1.csv
-../bin/mmult 2000 100 > ../data/n2000_r100_ord1.csv
+echo "200 done"
+../bin/mmult 500 20 > ../data/n500_r20_ord1.csv
+echo "500 done"
+../bin/mmult 1000 20 > ../data/n1000_r20_ord1.csv
+echo "1000 done"
+../bin/mmult 2000 10 > ../data/n2000_r10_ord1.csv
+echo "2000 done"
