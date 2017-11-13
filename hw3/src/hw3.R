eebb595c69f1709a6eaab41c987ea4cb9c7beda3
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-12 21:40:05 -0800

    reading simulation outputs into externalized reporting code

--- hw3/src/hw3.R
+++ hw3/src/hw3.R
@@ -2,11 +2,32 @@
 #
 # Analysis script for repeated matrix multiplications
 
-## @knitr READ
-df = read.table("../data/n50_r100_ord1.csv", header=T, sep=",")
-head(df)
+## @knitr SETUP
 
-## @knitr PLOT
-plot(df)
-hist(df$clocks)
-hist(df$time)
+data_dir = "../data/"
+simfiles_ord1 = c("n100_r1000_ord1.csv", "n200_r100_ord1.csv",
+                  "n500_r20_ord1.csv", "n1000_r20_ord1.csv",
+                  "n2000_r10_ord1.csv")
+
+simfiles_noOpt = c("n250_r50_ordAll.csv", "n100_r100_ordAll.csv")
+
+simfiles_opt2 = c("n100_r1000_ordAll_O2opt.csv", "n250_r500_ordAll_O2opt.csv",
+                  "n500_r200_ordAll_O2opt.csv", "n2000_r100_ordAll_O2opt.csv")
+
+simfiles_opt3 = c("n100_r1000_ordAll_O3opt.csv", "n250_r500_ordAll_O3opt.csv",
+                  "n500_r200_ordAll_O3opt.csv", "n2000_r100_ordAll_O3opt.csv")
+
+## @knitr READ_pr1
+df1 = data.frame(clocks=NA, time=NA, batch=NA)
+for(fn in simfiles_ord1){
+    fp = paste(data_dir, fn, sep="")
+    df = read.table(fp, header=T, sep=",")
+    df$batch = fn
+    df1 = rbind(df1, df)
+}
+df1 = na.omit(df1)
+
+## @knitr PLOT_pr1
+# plot(df)
+# hist(df$clocks)
+# hist(df$time)
